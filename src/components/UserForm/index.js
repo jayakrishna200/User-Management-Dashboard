dc0df87974e9fd1bd1e4039e071788a2a6237d3f
Jaya Krishna <jayakrishnagundupu@gmail.com>
2025-01-27 01:50:32 +0530

    Integrate:UserForm Component with App.js

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -9,8 +9,7 @@ class UserForm extends Component {
       firstName: '',
       lastName: '',
       email: '',
-      department: '',
-      errors: {}
+      department: ''
     };
   }
 
@@ -21,86 +20,97 @@ class UserForm extends Component {
     }
   }
 
-  handleBlur = (field) => {
-    if (!this.state[field]) {
-      this.setState((prevState) => ({
-        errors: {
-          ...prevState.errors,
-          [field]: 'This field is required'
-        }
-      }));
-    } else {
-      this.setState((prevState) => {
-        const errors = { ...prevState.errors };
-        delete errors[field];
-        return { errors };
-      });
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.validateForm()) {
+      this.props.onSubmit(this.state);
+      this.resetForm();
     }
   };
 
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
+  validateForm = () => {
+    const { firstName, lastName, email } = this.state;
+    if (!firstName || !lastName || !email) {
+      alert('All fields are required');
+      return false;
+    }
+    if (!this.validateEmail(email)) {
+      alert('Please enter a valid email');
+      return false;
+    }
+    return true;
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { id, firstName, lastName, email, department } = this.state;
-    const userData = { id, firstName, lastName, email, department };
-    this.props.onSubmit(userData);
+  validateEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  resetForm = () => {
+    this.setState({
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      department: ''
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
-    const { firstName, lastName, email, department, errors } = this.state;
+    const { show, onClose } = this.props;
+    if (!show) return null;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label>First Name</label>
-          <input
-            type="text"
-            name="firstName"
-            value={firstName}
-            onChange={this.handleChange}
-            onBlur={() => this.handleBlur('firstName')}
-          />
-          {errors.firstName && <span className="error">{errors.firstName}</span>}
+      <div className="modal">
+        <div className="modal-content">
+          <h2>{this.state.id ? 'Edit User' : 'Add User'}</h2>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>First Name:</label>
+              <input
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Last Name:</label>
+              <input
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Department:</label>
+              <input
+                type="text"
+                name="department"
+                value={this.state.department}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-actions">
+              <button type="submit">Submit</button>
+              <button type="button" onClick={onClose}>Cancel</button>
+            </div>
+          </form>
         </div>
-        <div>
-          <label>Last Name</label>
-          <input
-            type="text"
-            name="lastName"
-            value={lastName}
-            onChange={this.handleChange}
-            onBlur={() => this.handleBlur('lastName')}
-          />
-          {errors.lastName && <span className="error">{errors.lastName}</span>}
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-            onBlur={() => this.handleBlur('email')}
-          />
-          {errors.email && <span className="error">{errors.email}</span>}
-        </div>
-        <div>
-          <label>Department</label>
-          <input
-            type="text"
-            name="department"
-            value={department}
-            onChange={this.handleChange}
-            onBlur={() => this.handleBlur('department')}
-          />
-          {errors.department && <span className="error">{errors.department}</span>}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     );
   }
 }
